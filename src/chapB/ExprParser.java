531ec17adeb4c6dca8e8df5ddd7c5e8865c7efac
Lighters_c <2362894675@qq.com>
2017-10-12 19:35:49 +0800

    加减法语法分析器的注释

--- src/chapB/ExprParser.java
+++ src/chapB/ExprParser.java
@@ -21,6 +21,13 @@ public class ExprParser {
         lexer=p;
     }
 
+    /**
+     * 读取一个term(),term()一般为乘/除法表达式或者一些优先级更高的运算式子，然后判断下一个即将读取的是否是
+     * 加号或者减号，如果是的话就读取进来，然后再读取运算符(加号/减号)的右操作数，然后把整个加/减法表达式新建
+     * 为一个BinaryExpr的AST节点返回
+     * @return
+     * @throws ParseException
+     */
     public ASTree expression() throws ParseException {
         ASTree left=term();
         while(isToken("+") || isToken("-")) {
@@ -31,6 +38,13 @@ public class ExprParser {
         return left;
     }
 
+    /**
+     * 读取factor,一般这里的factor要么返回数字字面量，要么返回表达式，然后判断下一个即将读取的是不是乘除号
+     * 如果是的话，就将其读取，然后再读取右边的factor()，然后把整个乘/除法表达式新建为一个BinaryExpr的AST
+     * 节点并返回
+     * @return
+     * @throws ParseException
+     */
     public ASTree term() throws ParseException {
         ASTree left = factor();
         while(isToken("*")||isToken("/")) {
@@ -41,6 +55,14 @@ public class ExprParser {
         return left;
     }
 
+    /**
+     * factor()方法先判断即将读入的符号是不是"("，如果是，则调用token()读取左括号,然后按铁路图调用expression()
+     * 把括号中间的表达式读取进来，再调用token()读取右括号，返回括号中间的表达式。
+     * 如果一开始判断即将读入的不是"("，那就直接读取出来，根据铁路图，读到的一定是数字，所以就新建数字字面量并
+     * 返回，出错则抛出异常
+     * @return
+     * @throws ParseException
+     */
     public ASTree factor() throws ParseException {
         if(isToken("(")) {
             token("(");
@@ -58,12 +80,23 @@ public class ExprParser {
         }
     }
 
+    /**
+     * token()方法用来判断传入的符号从词法分析器中读取的是否一致,实际读取
+     * @param name
+     * @throws ParseException
+     */
     void token(String name) throws ParseException {
         Token t = lexer.read();
         if(!(t.isIdentifier()&&name.equals(t.getText())))
             throw new ParseException(t);
     }
 
+    /**
+     * 同token(),但不实际从词法分析器中读取
+     * @param name
+     * @return
+     * @throws ParseException
+     */
     boolean isToken(String name) throws ParseException {
         Token t= lexer.peek(0);
         return t.isIdentifier() && name.equals(t.getText());
